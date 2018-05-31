e6c74822863f8baeb96d71101de1e79f8050d259
Joanna Nelson <nels.joanna@gmail.com>
2018-05-31 01:07:33 -0400

    Update generatepassword.py

--- generatepassword.py
+++ generatepassword.py
@@ -4,9 +4,10 @@ chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
 
 length = input('Password length? ')
 length = int(length)
+howmany = input('How many passwords do you want? ')
+howmany = int(howmany)
 
-# for more than 4 passwords, change number below. for just one, comment out line 9 and fix spacing. 
-for p in range(4):
+for p in range(howmany):
     password = ''
     for c in range(length):
         password += random.choice(chars)
